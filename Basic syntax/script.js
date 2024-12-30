a557e5c767d89318b8d90945e48f6f11006ca75d
Devraj-jha <devrajjha100806@gmail.com>
2024-12-30 18:49:30 +0530

    Add new folder or update files

--- Basic syntax/script.js
+++ Basic syntax/script.js
@@ -3,4 +3,4 @@ console.log("hello world")
 git add .                                   # Stage all changes
 git commit -m "Add new folder or update files"  # Commit changes
 git push origin main                        # Push changes to GitHub (replace `main` with `master` if necessary)
-*\
+*/
