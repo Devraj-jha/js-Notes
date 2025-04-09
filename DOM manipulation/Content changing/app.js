8c48e1cee1c2b0c5f5ef75de219a03643b480c83
Devraj-jha <devrajjha100806@gmail.com>
2025-04-09 19:11:15 +0530

    dom

---
+++ DOM manipulation/Content changing/app.js
@@ -0,0 +1,8 @@
+const first = document.querySelector(".first");
+console.log(first.innerText);
+
+const second = document.querySelector(".second");
+console.log(second.textContent);
+const third = document.querySelector(".third")
+;
+console.log(third.innerHTML)
